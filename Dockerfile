e9e71bdacf18ce7a93b05da960c726bc93ff7a5b
mccullen <mccullen@mccullen.xyz>
2025-04-04 18:21:02 -0500

    add proxy thing for live reloading

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ FROM golang:latest
 RUN go install github.com/air-verse/air@latest
 
 WORKDIR /app
-COPY . .
-RUN go mod download
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
--- hellohttp.go
+++ hellohttp.go
@@ -6,8 +6,7 @@ import (
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-
-	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+	fmt.Fprintf(writer, "<html><body><h1>h1, very cool</h1><p>%s</p></body></html>", request.URL.Path[1:])
 }
 
 func main() {
@@ -15,7 +14,3 @@ func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
-
-// A note goes here
-
-// another note goes here
