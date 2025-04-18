27420ef9c5d8c8219c6cfca66c32d81c75f37ad1
mccullen <mccullen@mccullen.xyz>
2025-04-18 16:23:56 -0500

    add instructions

--- dev.sh
+++ dev.sh
@@ -1,4 +1,5 @@
 # development script, this is kinda redundant at the moment because our
 # dev and prod environments are exactly the same. Soon we'll separate out our
 # dev and prod into their own docker images, and then that will require a dev.sh
+echo "GO TO localhost:8081 🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡🫡"
 docker compose up --build
